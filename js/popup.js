6b203090ff8be2712d1786a861b0e5dda21015f2
Loovark <silverdragon999@yandex.ru>
2017-12-09 19:04:10 +0300

    Правки js 09.12.17

--- js/popup.js
+++ js/popup.js
@@ -3,23 +3,22 @@ var popup = document.querySelector(".search");
 var form = document.querySelector(".search-form");
 var guests = document.querySelector(".guests-number");
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("toggler-content-off");
-});
-
-form.addEventListener("submit", function (evt) {
-  if (!guests.value) {
+link.addEventListener("click", function(evt) {
     evt.preventDefault();
-    console.log("Нужно ввести количество гостей");
-  }
-  else {
-    localStorage.setItem(guests.value);
-  }
+    popup.classList.toggle("toggler-content-off");
 });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.toggle("toggler-content-off"));
-  }
+form.addEventListener("submit", function(evt) {
+    if (!guests.value) {
+        evt.preventDefault();
+        console.log("Нужно ввести количество гостей");
+    } else {
+        localStorage.setItem(guests.value);
+    }
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        if (popup.classList.toggle("toggler-content-off"));
+    }
 });
